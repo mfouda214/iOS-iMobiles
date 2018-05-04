ac54163aa5b931e367b02ce79ebe8b7917b257cd
mfouda214 <m.fouda@robone.net>
2018-05-04 18:53:31 +0100

    Edit mainView seguas

--- imobiles/ViewController.swift
+++ imobiles/ViewController.swift
@@ -19,6 +19,47 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Button start
+    
+    
+    @IBAction func TodoList(_ sender: Any) {
+        
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "todo")
+        
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: true, completion:nil)
+        
+        
+    }
+    
+    @IBAction func QRScan(_ sender: Any) {
+    
+    let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    let viewController = storyboard.instantiateViewController(withIdentifier: "scan")
+    
+    viewController.modalPresentationStyle = .fullScreen
+    present(viewController, animated: true, completion:nil)
+    
+    
+    }
+
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+    return .fullScreen
+    }
+
+    func presentationController(_ controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+    let navigationController = UINavigationController(rootViewController: controller.presentedViewController)
+    let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(ViewController.dismissViewController))
+    navigationController.topViewController?.navigationItem.rightBarButtonItem = doneButton
+    return navigationController
+    
+    }
+
+    @objc func dismissViewController() {
+    self.dismiss(animated: true, completion: nil)
+    }
 
 
 }
